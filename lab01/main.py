0fb5bc3e81d60502bbacd728560212464c258595
Kacper Kafara <kacperkafara@gmail.com>
2021-10-24 21:21:37 +0200

    Clean up lab01

--- lab01/main.py
+++ lab01/main.py
@@ -10,38 +10,40 @@ unicode_shift_uppercase = 65
 latin_alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 class CaesarUpgradedEncoder(object):
-    def __init__(self, in_alphabet: Alphabet = latin_alphabet, out_alphabet: Alphabet = None) -> None:
+    def __init__(self, key: int = 3, in_alphabet: Alphabet = latin_alphabet, out_alphabet: Alphabet = None) -> None:
         super().__init__()
         self.in_alphabet = in_alphabet
         self.out_alphabet = out_alphabet if out_alphabet is not None else in_alphabet
+        self.key = key
         
-    def __call__(self, plaintext: Plaintext, shift: int = 0):
+    def __call__(self, plaintext: Plaintext):
         ciphertext = ["" for _ in range(len(plaintext))]
         for i, char in enumerate(plaintext.lower().strip()):
-            ciphertext[i] = self.out_alphabet[(ord(char) - unicode_shift_lowercase + shift) % len(self.out_alphabet)]
+            ciphertext[i] = self.out_alphabet[(ord(char) - unicode_shift_lowercase + self.key) % len(self.out_alphabet)]
         return "".join(ciphertext)
 
 
 class CaesarUpgradedDecoder(object):
-    def __init__(self, in_alphabet: Alphabet = latin_alphabet, out_alphabet: Alphabet = None) -> None:
+    def __init__(self, key: int = 3, in_alphabet: Alphabet = latin_alphabet, out_alphabet: Alphabet = None) -> None:
         super().__init__()
         self.in_alphabet = in_alphabet
         self.out_alphabet = out_alphabet if out_alphabet is not None else in_alphabet
+        self.key = key
 
-    def __call__(self, ciphertext: Ciphertext, shift: int = 0):
+    def __call__(self, ciphertext: Ciphertext):
         plaintext = ["" for _ in range(len(ciphertext))]
         for i, char in enumerate(ciphertext.lower().strip()):
-            plaintext[i] = self.in_alphabet[(ord(char) - unicode_shift_lowercase - shift) % len(self.in_alphabet)]
+            plaintext[i] = self.in_alphabet[(ord(char) - unicode_shift_lowercase - self.key) % len(self.in_alphabet)]
         return "".join(plaintext)
 
 
-class CaesarUpgraded(object):
+class Cipher(object):
     def __init__(
         self, 
+        encoder = CaesarUpgradedEncoder(), 
+        decoder = CaesarUpgradedDecoder(),
         in_alphabet: Alphabet = latin_alphabet,
         secret_alphabet: Alphabet = None, 
-        encoder = CaesarUpgradedEncoder(), 
-        decoder = CaesarUpgradedDecoder()
     ) -> None:
         self.in_alphabet = in_alphabet
         self.secret_alphabet = secret_alphabet if secret_alphabet is not None else in_alphabet
@@ -52,11 +54,15 @@ class CaesarUpgraded(object):
         self.decoder.in_alphabet = self.secret_alphabet
         self.decoder.out_alphabet = self.in_alphabet
 
-    def encode(self, plaintext: Plaintext, shift: int = 0) -> Ciphertext:
-        return self.encoder(plaintext, shift)
+    def encode(self, plaintext: Plaintext) -> Ciphertext:
+        return self.encoder(plaintext)
     
-    def decode(self, ciphertext: Ciphertext, shift: int = 0) -> Plaintext:
-        return self.decoder(ciphertext, shift)
+    def decode(self, ciphertext: Ciphertext) -> Plaintext:
+        return self.decoder(ciphertext)
+    
+
+
+class CaesarUpgraded(object):
 
 
 def main():
