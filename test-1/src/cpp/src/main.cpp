be663f189154ae8331ab0ab9b335168784ef4fee
Kacper Kafara <kacperkafara@gmail.com>
2021-12-07 15:33:57 +0100

    Update

--- test-1/src/cpp/src/main.cpp
+++ test-1/src/cpp/src/main.cpp
@@ -22,19 +22,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cstring>
+#include <bitset>
+#include <memory>
 
-typedef uint8_t byte_t;
 
 #define N_ROUNDS 16
 #define BLOCK_SIZE_b 64
 #define BLOCK_SIZE_B (BLOCK_SIZE_b / 8)
 #define HALF_BLOCK_SIZE_B (BLOCK_SIZE_B / 2)
+#define HALF_BLOCK_SIZE_b (BLOCK_SIZE_b / 2)
 #define KEY_SIZE_b 64
 #define KEY_SIZE_B (KEY_SIZE_b / 8)
 
-constexpr byte_t pc_1[] = {
+typedef uint8_t byte_t;
+// typedef std::bitset<KEY_SIZE_b> key_type;
 
+constexpr byte_t key_permutation[] = {
+	45, 31, 32,  1, 63, 19, 62, 53,
+	 2,  9, 11, 21, 37, 51, 54, 18,
+	34, 47, 16,  8,  0, 52, 43, 41,
+	 6, 49, 33, 22, 27, 44, 24, 57,
+	 3, 20, 28, 60, 61, 39, 50, 25,
+	17, 10,  4,  5, 58, 14, 29, 38,
+	12, 35, 59, 13, 46, 23, 42, 26,
+	36, 56, 30,  7, 48, 55, 15, 40
 };
+
 
 constexpr byte_t block_extension_map[] = {
   27, 28, 29, 30, 31,  0,  1,  2,
@@ -168,20 +181,26 @@ constexpr byte_t sbox[16][4][4] = {
 	}
 };
 
-inline void _XOR_(
-  const byte_t bytes1[],
-  const byte_t bytes2[],
-  byte_t result[],
-  size_t size
-) {
-  for (size_t i = 0; i < size; ++i) {
-    result[i] = bytes1[i] ^ bytes2[i];
+
+inline void print_text(const byte_t * const text, size_t len) {
+  for (size_t i = 0; i < len; ++i)
+    std::cout << (char) text[i];
+
+  std::cout << std::endl;
+}
+
+std::unique_ptr<std::bitset<KEY_SIZE_b>[]> generate_keys(const std::bitset<KEY_SIZE_b>& key) {
+	std::unique_ptr<std::bitset<KEY_SIZE_b>[]> key_set(new std::bitset<KEY_SIZE_b>[N_ROUNDS]);
+
+	for (size_t i = 0; i < N_ROUNDS; ++i) {
+		for (size_t j = 0; j < KEY_SIZE_b; ++j) {
+			// key_set[i][j] = key[key_permutation[j]];
+			key_set[i][j] = key[j];
+		}
 	}
+	return key_set;
 }
 
-void generate_keys(byte_t initial_key[KEY_SIZE_B], byte_t key_set[N_ROUNDS][KEY_SIZE_B]) {
-
-}
 
 inline void _AND_(const byte_t bytes[], const byte_t mask[], byte_t output[], size_t byte_count) {
 	for (size_t i = 0; i < byte_count; ++i) {
@@ -196,154 +215,195 @@ inline void _OR_(const byte_t bytes[], const byte_t mask[], byte_t output[], siz
 }
 
 
-void apply_sbox(byte_t input[BLOCK_SIZE_B], byte_t output[HALF_BLOCK_SIZE_B]) {
-	memset(output, 0, HALF_BLOCK_SIZE_B);
-	// dzielimy 64 bity wejściowe na 16 części
-	byte_t mask_l = 0b1111'0000, mask_r = 0b0000'1111;
-	byte_t halfbyte_ll, halfbyte_lr, halfbyte_rl, halfbyte_rr;
-	byte_t mask_row = 0b0101, mask_col = 0b1010;
-	byte_t twobit_ll, twobit_lr, twobit_rl, twobit_rr;
+std::bitset<HALF_BLOCK_SIZE_b> apply_sbox(
+	const std::bitset<BLOCK_SIZE_b>& input
+) {
+	std::bitset<HALF_BLOCK_SIZE_b> output(0);
+	std::bitset<4> input_block, row_mask(0b0101), col_mask(0b1010);
+	byte_t sbox_out;
+	for (size_t i = 0, j = 0; i < BLOCK_SIZE_b; i += 4, j += 2) {
+		input_block[0] = input[i];
+		input_block[1] = input[i + 1];
+		input_block[2] = input[i + 2];
+		input_block[3] = input[i + 3];
 
-	for (size_t i = 0, j = 0; i < BLOCK_SIZE_B; i += 2, ++j) {
-		halfbyte_ll = (input[i] & mask_l) >> 4;
-		halfbyte_lr = input[i] & mask_r;
-		halfbyte_rl = (input[i + 1] & mask_l) >> 4;
-		halfbyte_rr = input[i + 1] & mask_r;
-
-		twobit_ll = sbox[2 * i][halfbyte_ll & mask_row][halfbyte_ll & mask_col];
-		twobit_lr = sbox[2 * i + 1][halfbyte_lr & mask_row][halfbyte_lr & mask_col];
-		twobit_rl = sbox[2 * i + 2][halfbyte_rl & mask_row][halfbyte_rl & mask_col];
-		twobit_rr = sbox[2 * i + 3][halfbyte_rr & mask_row][halfbyte_rr & mask_col];
-
-		output[j] = (twobit_ll << 6) | (twobit_lr << 4) | (twobit_rl << 2) | (twobit_rr);
+		sbox_out = sbox[j >> 1][(input_block & row_mask).to_ulong()][(input_block & col_mask).to_ulong()];
+		output[j] = sbox_out & 2;
+		output[j + 1] = sbox_out & 1;
 	}
-
-	
+	return output;
 }
 
-void nonlinear_f(const byte_t key[KEY_SIZE_B], const byte_t bytes[HALF_BLOCK_SIZE_B], byte_t output[HALF_BLOCK_SIZE_B]) {
-  byte_t ext_bytes[BLOCK_SIZE_B];
-  for (size_t i = 0; i < BLOCK_SIZE_B; ++i)
-    ext_bytes[i] = bytes[block_extension_map[i]];
-  
-  _XOR_(ext_bytes, key, ext_bytes, BLOCK_SIZE_B);
+std::bitset<HALF_BLOCK_SIZE_b> nonlinear_f(
+	const std::bitset<KEY_SIZE_b>& key,
+	const std::bitset<HALF_BLOCK_SIZE_b>& bits
+) {
+	std::bitset<BLOCK_SIZE_b> extbits;
+	for (size_t i = 0; i < BLOCK_SIZE_b; ++i) {
+		extbits[i] = bits[block_extension_map[i]];
+	}
 
-	apply_sbox(ext_bytes, output);
+	extbits ^= key; 
+
+	return apply_sbox(extbits);
 }
 
 
 void encode(
-  const char * const plain_text, 
-  size_t plain_text_len, 
-  char * const cipher_text_buffer,
-  byte_t key[KEY_SIZE_B]
+  const byte_t * const plain_text, 
+  size_t plain_text_size_B, 
+  byte_t * const cipher_text_buffer,
+  const std::bitset<KEY_SIZE_b>& key
 ) {
-  memset(cipher_text_buffer, 0, plain_text_len);
+  memset(cipher_text_buffer, 0, plain_text_size_B);
 
-  size_t block_count = plain_text_len / BLOCK_SIZE_b;
+  size_t block_count = plain_text_size_B / BLOCK_SIZE_B;
 
-  byte_t key_set[N_ROUNDS][KEY_SIZE_B];
-  generate_keys(key, key_set);
+	std::cout << "Liczba bloków do zakodowania: " << block_count << '\n';
 
-  byte_t block_l[HALF_BLOCK_SIZE_B];
-  byte_t block_r[HALF_BLOCK_SIZE_B];
-  // byte_t new_block_l[HALF_BLOCK_SIZE_B];
-  byte_t new_block_r[HALF_BLOCK_SIZE_B];
+	std::unique_ptr<std::bitset<KEY_SIZE_b>[]> key_set = generate_keys(key);
 
-	byte_t f_output[HALF_BLOCK_SIZE_B];
+	std::cout << "key set:\n";
+	for (int i = 0; i < N_ROUNDS; ++i) {
+		std::cout << key_set[i] << '\n';
+	}
+
+
+	// std::bitset<HALF_BLOCK_SIZE_b> block_l;
+	std::bitset<HALF_BLOCK_SIZE_b> block_r;
+	std::bitset<HALF_BLOCK_SIZE_b> new_block_r;
+	uint32_t block_value;
 
   for (size_t block_i = 0; block_i < block_count; ++block_i) {
     // dzielę blok na pół
-    memcpy(
-      block_l, 
-      plain_text + block_i * BLOCK_SIZE_B, 
-      HALF_BLOCK_SIZE_B
-    );
-    memcpy(
-      block_r, 
-      plain_text + block_i * BLOCK_SIZE_B + HALF_BLOCK_SIZE_B, 
-      HALF_BLOCK_SIZE_B
-    );
+		print_text(plain_text + block_i * BLOCK_SIZE_B, BLOCK_SIZE_B);
+		memcpy(&block_value, plain_text + block_i * BLOCK_SIZE_B, HALF_BLOCK_SIZE_B);
+		std::bitset<HALF_BLOCK_SIZE_b> block_l(block_value);
+		memcpy(&block_value, plain_text + block_i * BLOCK_SIZE_B + HALF_BLOCK_SIZE_B, HALF_BLOCK_SIZE_B);
+		std::bitset<HALF_BLOCK_SIZE_b> block_r(block_value);
+
+		std::cout << "L_i: ";
+		std::cout << block_l;
+		std::cout << "   R_i: ";
+		std::cout << block_r << '\n';
 
     for (size_t round_i = 0; round_i < N_ROUNDS; ++round_i) {
-			nonlinear_f(key_set[round_i], block_r, f_output);
-      _XOR_(
-        block_l,
-				f_output,
-        new_block_r,
-        HALF_BLOCK_SIZE_B
-        );
-      memcpy(block_l, block_r, HALF_BLOCK_SIZE_B);
-      memcpy(block_r, new_block_r, HALF_BLOCK_SIZE_B);
+			std::bitset<HALF_BLOCK_SIZE_b> new_block_r = block_l ^ nonlinear_f(key_set[round_i], block_r);
+			block_l = block_r;
+			block_r = new_block_r;
     }
     // skopiowanie do wyniku
+		block_value = block_l.to_ulong();
     memcpy(
       cipher_text_buffer + block_i * BLOCK_SIZE_B,
-      block_l,
+      &block_value,
       HALF_BLOCK_SIZE_B
     );
+		block_value = block_r.to_ulong();
     memcpy(
       cipher_text_buffer + block_i * BLOCK_SIZE_B + HALF_BLOCK_SIZE_B,
-      block_r,
+      &block_value,
       HALF_BLOCK_SIZE_B
     );
   }
 }
 
 void decode(
-  const char * const cipher_text,
-  size_t cipher_text_len,
-  char * const plain_text_buffer,
-  byte_t key[KEY_SIZE_B]
+  const byte_t * const cipher_text,
+  size_t cipher_text_size_B,
+  byte_t * const plain_text_buffer,
+  std::bitset<KEY_SIZE_b>& key
 ) {
-  memset(plain_text_buffer, 0, cipher_text_len);
+  memset(plain_text_buffer, 0, cipher_text_size_B);
+
+  size_t block_count = cipher_text_size_B / BLOCK_SIZE_B;
+
+	std::cout << "Liczba bloków do zakodowania: " << block_count << '\n';
+
+	std::unique_ptr<std::bitset<KEY_SIZE_b>[]> key_set = generate_keys(key);
+
+	std::cout << "key set:\n";
+	for (int i = 0; i < N_ROUNDS; ++i) {
+		std::cout << key_set[i] << '\n';
+	}
+
+
+	// std::bitset<HALF_BLOCK_SIZE_b> block_l;
+	std::bitset<HALF_BLOCK_SIZE_b> block_r;
+	std::bitset<HALF_BLOCK_SIZE_b> new_block_r;
+	uint32_t block_value;
+
+  for (size_t block_i = 0; block_i < block_count; ++block_i) {
+    // dzielę blok na pół
+		memcpy(&block_value, cipher_text + block_i * BLOCK_SIZE_B, HALF_BLOCK_SIZE_B);
+		std::bitset<HALF_BLOCK_SIZE_b> block_l(block_value);
+		memcpy(&block_value, cipher_text + block_i * BLOCK_SIZE_B + HALF_BLOCK_SIZE_B, HALF_BLOCK_SIZE_B);
+		std::bitset<HALF_BLOCK_SIZE_b> block_r(block_value);
+
+		std::cout << "L_i: ";
+		std::cout << block_l;
+		std::cout << "   R_i: ";
+		std::cout << block_r << '\n';
+
+    for (size_t round_i = 0; round_i < N_ROUNDS; ++round_i) {
+			std::bitset<HALF_BLOCK_SIZE_b> new_block_r = block_l ^ nonlinear_f(key_set[N_ROUNDS - round_i - 1], block_r);
+			block_l = block_r;
+			block_r = new_block_r;
+    }
+    // skopiowanie do wyniku
+		block_value = block_l.to_ulong();
+    memcpy(
+      plain_text_buffer + block_i * BLOCK_SIZE_B,
+      &block_value,
+      HALF_BLOCK_SIZE_B
+    );
+		block_value = block_r.to_ulong();
+    memcpy(
+      plain_text_buffer + block_i * BLOCK_SIZE_B + HALF_BLOCK_SIZE_B,
+      &block_value,
+      HALF_BLOCK_SIZE_B
+    );
+  }
 }
-
-inline void print_text(const char * const text, size_t len) {
-  for (size_t i = 0; i < len; ++i)
-    std::cout << text[i];
-
-  std::cout << std::endl;
-}
-
-// inline void set
-
 
 int main(int argc, char * argv[]) {
-  constexpr int text_size = 512;
+  constexpr int text_size_B = 64;
 
   srand(time(NULL));
 
-  byte_t key[KEY_SIZE_B] = {
-		0b1011'1001,
-		0b0000'1101,
-		0b1111'1100,
-		0b1010'0110,
-		0b0010'1111,
-		0b1011'1110,
-		0b0001'0101,
-		0b0111'0011
-	};
+	std::bitset<KEY_SIZE_b> key (
+		std::string("11010101101000111101100010010100110011111111111100110101010100011")
+	);
 
-  char * const plain_text = new char[text_size];
-  char * const cipher_text = new char[text_size];
-  char * const decoded_text = new char[text_size];
 
-  for (int i = 0; i < text_size; ++i) {
+
+	std::cout << "Klucz wyjściowy: ";
+	std::cout << key << '\n' << key.size() << '\n';
+
+
+  byte_t * const plain_text = new byte_t[text_size_B];
+  byte_t * const cipher_text = new byte_t[text_size_B];
+  byte_t * const decoded_text = new byte_t[text_size_B];
+
+	memset(cipher_text, 0, text_size_B);
+	memset(decoded_text, 0, text_size_B);
+
+  for (int i = 0; i < text_size_B; ++i) {
     plain_text[i] = rand() % ('z' - 'a') + 'a';
   }
-  std::cout << "Plaintext:\n";
-  print_text(plain_text, text_size);
 
-  encode(plain_text, text_size, cipher_text, key);
+  std::cout << "Plaintext:\n";
+  print_text(plain_text, text_size_B);
+
+  encode(plain_text, text_size_B, cipher_text, key);
 
   std::cout << "Ciphertext:\n";
-  print_text(cipher_text, text_size);
+  print_text(cipher_text, text_size_B);
 
-  decode(cipher_text, text_size, decoded_text, key);
+  decode(cipher_text, text_size_B, decoded_text, key);
 
   std::cout << "Decoded:\n";
-  print_text(decoded_text, text_size);
+  print_text(decoded_text, text_size_B);
 
   delete[] plain_text;
   delete[] cipher_text;
